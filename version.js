defd3c6632269c8b560c5f4b0f364430fa7c1d3e
Shaw Innes <shaw@immortal.net.au>
2018-03-12 15:14:28 +1000

    Test Sequence Increment

--- version.js
+++ version.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
-import MongooseVersion from 'mongoose-version2';
-import MongooseSequence from 'mongoose-sequence';
+const MongooseSequence = require('mongoose-sequence')(mongoose);
 
 var Schema = mongoose.Schema;
 var _ = require('lodash');
@@ -11,92 +10,87 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('connected to mongodb');
+    console.log('connected to mongodb');
 });
 mongoose.connect('mongodb://localhost/mongoose-version');
 
 var PageSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  path: { type: String, required: true, unique: true },
-  user: { type: String, required: true },
-  tags: [String]
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    path: { type: String, required: true },
+    user: { type: String, required: true },
+    tags: [String]
 });
 
-PageSchema.plugin(MongooseVersion);
 PageSchema.plugin(MongooseSequence, { id: 'comment_seq', inc_field: 'commentNumber', reference_fields: ['user'] });
 
 var Page = mongoose.model('Page', PageSchema);
 
 var p1 = new Page({
-  title: 'Page one',
-  content: 'Lorem ipsum dolor sit amet',
-  user: 'user1',
-  path: '/page-one',
+    title: 'Page one',
+    content: 'Lorem ipsum dolor sit amet',
+    user: 'user1',
+    path: '/page-one',
 }).save().then((doc) => {
-  console.log('created ' + doc._id);
+    console.log('created ' + doc._id);
 }).catch((err) => {
-  console.log('error ' + err.errmsg);
+    console.log('error ' + err.errmsg);
 });
 
 var p2 = new Page({
-  title: 'Page two',
-  content: 'Lorem ipsum dolor sit amet',
-  user: 'user1',
-  path: '/page-two',
+    title: 'Page two',
+    content: 'Lorem ipsum dolor sit amet',
+    user: 'user1',
+    path: '/page-two',
 }).save().then((doc) => {
-  console.log('created ' + doc._id);
+    console.log('created ' + doc._id);
 }).catch((err) => {
-  console.log('error ' + err.errmsg);
+    console.log('error ' + err.errmsg);
 });
 
 var p3 = new Page({
-  title: 'Page three',
-  content: 'Lorem ipsum dolor sit amet',
-  user: 'user2',
-  path: '/page-three',
+    title: 'Page three',
+    content: 'Lorem ipsum dolor sit amet',
+    user: 'user2',
+    path: '/page-three',
 }).save().then((doc) => {
-  console.log('created ' + doc._id);
+    console.log('created ' + doc._id);
 }).catch((err) => {
-  console.log('error ' + err.errmsg);
+    console.log('error ' + err.errmsg);
 });
 
-Page.findOne({ path: /^\/page-one/ }, function (err, result) {
-  if (err) {
-    console.log('error retrieving document');
-    return;
-  }
+/*
+Page.findOne({ path: /^\/page-one/ }, function(err, result) {
+    if (err) {
+        console.log('error retrieving document');
+        return;
+    }
+    console.log('document1', result);
 
-  result.content = result.content.toUpperCase();
-  result.content = result.content + ' appended text';
+    result.content = result.content.toUpperCase();
+    result.content = result.content + ' appended text';
 
-  if (result.isModified()) {
-    console.log('document is modified, saving');
-    result.save();
-  } else {
-    console.log('it\'s okay, nothing really changed so I won\'t save');
-  }
+    if (result.isModified()) {
+        console.log('document is modified, saving');
+        result.save();
+    } else {
+        console.log('it\'s okay, nothing really changed so I won\'t save');
+    }
 });
 
-Page.findOne({ path: /^\/page-two/ }, function (err, result) {
-  if (err) {
-    console.log('error retrieving document');
-    return;
-  }
+Page.findOne({ path: /^\/page-two/ }, function(err, result) {
+    if (err) {
+        console.log('error retrieving document');
+        return;
+    }
+    console.log('document2', result);
 
-  result.content = result.content + ' appended text';
-  if (result.isModified()) {
-    console.log('document is modified, saving');
-    result.save();
-  } else {
-    console.log('it\'s okay, nothing really changed so I won\'t save');
-  }
+    result.content = result.content + ' appended text';
+    if (result.isModified()) {
+        console.log('document is modified, saving');
+        result.save();
+    } else {
+        console.log('it\'s okay, nothing really changed so I won\'t save');
+    }
 });
-
-var PageVersions = Page.VersionModel;
-
-PageVersions.find({  }, function (err, results) {
-  _.each(_.orderBy(results, ['doc.path', 'doc._v'], ['asc', 'asc']), function (item) {
-    console.log('document version ', item.doc.path, item.doc._id, item.doc._v, item.doc.tags);
-  });
-});
+*/
